02adf0ca294f063fbd8786c396278bbe8278e040
Niklas Voss <niklas.voss@gmail.com>
2023-08-15 19:46:46 +0200

    Basic nix environment for go development

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "zanzigo";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    let
+      overlays = [
+        # none, yet
+      ];
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      rec {
+        devShell = pkgs.mkShell rec {
+          name = "zanzigo";
+
+          buildInputs = with pkgs; [
+            go
+            golangci-lint
+            delve
+            gnumake
+          ];
+        };
+      }
+    );
+}
